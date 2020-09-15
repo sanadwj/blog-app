5770be20a56869b23e45eb964a2db89e79e1bdfd
wrakc <carlosveiga1618@gmail.com>
2020-09-15 18:07:17 -0300

    generate controller articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_191409) do
+ActiveRecord::Schema.define(version: 2020_09_15_203409) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
