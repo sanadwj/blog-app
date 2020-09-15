8946eccbffded7389678e7182e28d35145643c99
Sanad <s_wj_88@hotmail.com>
2020-09-16 01:32:53 +0300

    Add new.html.erb & Add ne & greate def

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,16 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.save
+    redirect_to article_path(@article)
+  end
   
 end
