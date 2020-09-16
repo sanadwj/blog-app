6c3bfbdc45e52bb3d11c58c1336f64fe0db91af6
Sanad <s_wj_88@hotmail.com>
2020-09-16 19:12:32 +0300

    Fix the paht Add edit.html.rb Add _form.html.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,17 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+
   
   def destroy
     @article = Article.find(params[:id])
     # flash.notice = "Article '#{@article.title}' Destroyed!"
     @article.destroy
-    redirect_to article_path
+    redirect_to articles_path
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
   end
 end
