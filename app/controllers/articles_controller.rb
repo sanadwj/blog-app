a8f1910ed2513f40718d2ff00494bb9ac79a173e
Sanad <s_wj_88@hotmail.com>
2020-09-16 19:27:25 +0300

    Add flash to the layout Add flash to the methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,19 +16,28 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
-  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def destroy
     @article = Article.find(params[:id])
-    # flash.notice = "Article '#{@article.title}' Destroyed!"
+    flash.notice = "Article '#{@article.title}' Destroyed!"
     @article.destroy
+    
     redirect_to articles_path
   end
 
-
-  def edit
+  def update
     @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+  
+    redirect_to article_path(@article)
   end
 end
